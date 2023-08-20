6016b1cc7c31ccf85b5b933be91405d746ca43a8
tomo-ki <69376551+tomo-ki@users.noreply.github.com>
2023-08-20 23:53:29 +0900

    19

---
+++ src/script.js
@@ -0,0 +1,24 @@
+// データベースにアクセスするためのクライアントライブラリ
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description: "GraphQLチュートリアルをUdemyで学ぶ",
+      url: "www.udemy-graphql-tutorial.com",
+    },
+  });
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    // データベースを閉じる
+    prisma.$disconnect;
+  });
