3bdc4d0c57eae30ce962bebf6b8c444bfd6e56e5
tomo-ki <69376551+tomo-ki@users.noreply.github.com>
2023-08-20 18:42:27 +0900

    8

--- src/server.js
+++ src/server.js
@@ -1,3 +1,15 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server");
 
 // GraphQLスキーマ(データ構造)の定義
+const typeDefs = gql`
+  type Query {
+    info: String!
+  }
+`;
+
+// resolver関数：typeDefsで定義した型に何か値を代入する関数
+const resolvers = {
+  Query: {
+    info: () => "HackerNewsクローン",
+  },
+};
