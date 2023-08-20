43450c62dab8f224df225c3bef4843b4dee8de82
tomo-ki <69376551+tomo-ki@users.noreply.github.com>
2023-08-20 19:02:22 +0900

    11

--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+// HackerNewsの1つ1つの投稿
+let links = [
+  {
+    id: "link-0",
+    description: "GraphQLチュートリアルをUdemyで学ぶ",
+    url: "www.udemy-graphql-tutorial.com",
+  },
+];
+
 // GraphQLスキーマ(データ構造)の定義
 // https://www.apollographql.com/docs/apollo-server/getting-started#step-3-define-your-graphql-schema
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
@@ -13,6 +29,7 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     info: () => "HackerNewsクローン",
+    feed: () => links,
   },
 };
 
