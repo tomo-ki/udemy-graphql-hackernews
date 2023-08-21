e2434e0a0ca677fe9413859338908c75a188517e
tomo-ki <69376551+tomo-ki@users.noreply.github.com>
2023-08-21 18:37:14 +0900

    37

---
+++ src/resolvers/Link.js
@@ -0,0 +1,10 @@
+// 誰によって投稿されたのか、のresolver
+function postedBy(parent, args, context) {
+  return context.prisma.link
+    .findUnique({ where: { id: parent.id } })
+    .postedBy();
+}
+
+module.exports = {
+  postedBy,
+};
